d670a90ded03e8cbcb772f7f84feb1f7ece91b8f
Peter Vrancx <pvrancx@vub.ac.be>
2017-06-02 11:47:27 +0200

    small model

--- planet/model.py
+++ planet/model.py
@@ -85,3 +85,37 @@ class PlanetNetLight(nn.Module):
         x = x.view(x.size(0), 32 * 6 * 6)
         x = self.classifier(x)
         return x
+
+
+class PlanetNetSmall(nn.Module):
+    def __init__(self,num_labels, dropout=0.4):
+        super(PlanetNetSmall, self).__init__()
+        self.cfg = {'dropout': dropout}
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=11, stride=4, padding=2),
+            nn.BatchNorm2d(8),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(8, 24, kernel_size=3, padding=1),
+            nn.BatchNorm2d(24),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(24, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(32 * 13 * 13, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, num_labels),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 32 * 13 * 13)
+        x = self.classifier(x)
+        return x
